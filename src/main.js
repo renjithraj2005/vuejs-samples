8e51adf96d792e087098052dec25ab3122edd377
Renjith Raj <renjithraj2005@gmail.com>
2020-07-04 23:13:27 +0530

    builder

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+// eslint-disable-next-line no-new
 new Vue({
+  el: '#app',
   render: (h) => h(App),
-}).$mount('#app');
+  created() {
+    console.log('created');
+  },
+});
+// .$mount('#app'); This is required only if the in stance doesn't have el option
