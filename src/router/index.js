113de95cf04dfeb79a13823b0e39732257550113
Renjith Raj <renjithraj2005@gmail.com>
2020-07-06 23:01:19 +0530

    view routing

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
 
 Vue.use(Router);
 
@@ -15,6 +20,28 @@ export default new Router({
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [{
+      path: 'heads',
+      name: 'BrowseHeads',
+      component: RobotHeads,
+    }, {
+      path: 'arms',
+      name: 'BrowseArms',
+      component: RobotArms,
+    }, {
+      path: 'bases',
+      name: 'BrowseBases',
+      component: RobotBases,
+    }, {
+      path: 'torsos',
+      name: 'BrowseTorsos',
+      component: RobotTorsos,
+    },
+    ],
   }, {
     path: '/parts/:partType/:id',
     name: 'Parts',
